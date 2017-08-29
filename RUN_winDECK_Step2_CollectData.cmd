f491191c508f57e5662623c59692beba73813f54
StefanKubsch <31415283+StefanKubsch@users.noreply.github.com>
2017-08-29 19:46:16 +0200

    Change comment

--- RUN_winDECK_Step2_CollectData.cmd
+++ RUN_winDECK_Step2_CollectData.cmd
@@ -1,6 +1,6 @@
+@ECHO OFF
 REM Runs script with elevated rights
 
-@ECHO OFF
 SET ScriptDir=%~dp0
 SET PSScriptPath=%ScriptDir%winDECK_Module_CollectData.ps1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PSScriptPath%""' -Verb RunAs}";
